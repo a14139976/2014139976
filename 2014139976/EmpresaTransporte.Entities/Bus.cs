42fb32ef1d9b3545fdd6626cdbbf3d6c47efadc7
a14139976 <luiz_27_20@hotmail.com>
2017-04-14 13:22:10 -0500

    commit de prueba

---
+++ 2014139976/EmpresaTransporte.Entities/Bus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpresaTransporte.Entities
+{
+    public class Bus
+    {
+        public int codChofer;
+        public int nroPlaca;
+        public int nroBus;
+        public int nroAsientos;
+    }
+}
---
+++ 2014139976/EmpresaTransporte.Entities/LugarViaje.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpresaTransporte.Entities
+{
+    public class LugarViaje
+    {
+        public String nombre;
+        public String direccion;
+        public TipoLugar tipolugar;
+        public String telefono;
+    }
+}
